a7eeaf7cc2b4d3bb7fa2a2b8d9c3c0502b6bbf0d
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-05-03 22:44:27 +0300

    add working with cart

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from random import choice
 
 import requests
 from dotenv import load_dotenv
@@ -23,11 +24,45 @@ def get_product_ids(token):
     return product_ids
 
 
+def add_to_cart(token, product_id):
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+
+    json_data = {
+        'data': {
+            'id': product_id,
+            'type': 'cart_item',
+            'quantity': 1,
+        },
+    }
+
+    response = requests.post('https://api.moltin.com/v2/carts/abc/items', headers=headers, json=json_data)
+    response.raise_for_status()
+
+    print(response.text)
+
+
+def get_cart(token):
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+
+    response = requests.get('https://api.moltin.com/v2/carts/abc', headers=headers)
+    print(response.text)
+
+
 def main():
     load_dotenv()
     shop_token = os.getenv('SHOP_ACCESS_TOKEN')
 
-    product_ids = get_product_ids(shop_token)
+    # product_ids = get_product_ids(shop_token)
+
+    # add_to_cart(shop_token, choice(product_ids))
+
+    get_cart(shop_token)
+
+
 
 
 
